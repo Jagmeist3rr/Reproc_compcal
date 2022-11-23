d49cd23522e509234bb1b0efa54981236ca95d2a
Jagmeister <unsaid-09.imagers@icloud.com>
2022-11-23 16:54:20 -0500

    Fixed spacing inside of each ore dict

--- Mineral_classes.py
+++ Mineral_classes.py
@@ -8,42 +8,43 @@ class Mineral():
     def ore_dict_print(ore_dict):
         return ore_dict
 
+
 class Tritanium(Mineral):
-    ore_dict= {"Veldspar": 1.25, "Scordite": .49, "Plagioclase": .15, "Omber": 1.8, "Kernite": .8, "Pyroxeres": 5.26,
-               "Dark Orchre": 2.88, "Spodumain": 59.1, "Hemorphite": 16.5, "Crokite": 116.4, "Arkonor": 26.4,
-               "Tritanium": 0}
+    ore_dict = {"Veldspar": 1.25, "Scordite": .49, "Plagioclase": .15, "Omber": 1.8, "Kernite": .8, "Pyroxeres": 5.26,
+                "Dark Orchre": 2.88, "Spodumain": 59.1, "Hemorphite": 16.5, "Crokite": 116.4, "Arkonor": 26.4,
+                "Tritanium": 0}
 
 
 class Pyerite(Mineral):
-    ore_dict= {"Scordite": .34, "Plagioclase": .2, "Omber": .23, "Pyroxeres": 1.68, "Gneiss": 2.64,
-               "Spodumain": 11.22, "Hemorphite": 8.19, "Bistot": 18.36, "Pyerite": 0}
+    ore_dict = {"Scordite": .34, "Plagioclase": .2, "Omber": .23, "Pyroxeres": 1.68, "Gneiss": 2.64,
+                "Spodumain": 11.22, "Hemorphite": 8.19, "Bistot": 18.36, "Pyerite": 0}
 
 
 class Mexallon(Mineral):
-    ore_dict= {"Plagioclase": .29, "Kernite": 1.44, "Pyroxeres": 0.75, "Gneiss": 2.76,
-               "Spodumain": 1.08, "Jaspet": 7.38, "Arkonor": 3.0,"Mexallon": 0}
+    ore_dict = {"Plagioclase": .29, "Kernite": 1.44, "Pyroxeres": 0.75, "Gneiss": 2.76,
+                "Spodumain": 1.08, "Jaspet": 7.38, "Arkonor": 3.0, "Mexallon": 0}
 
 
 class Isogen(Mineral):
-    ore_dict= {"Omber": 0.17, "Kernite": .14, "Dark Orchre": 0.17, "Gneiss": 0.55,
-               "Spodumain": .18, "Hemorphite": 0.48, "Hedbergite": 1.38,"Isogen": 0}
+    ore_dict = {"Omber": 0.17, "Kernite": .14, "Dark Orchre": 0.17, "Gneiss": 0.55,
+                "Spodumain": .18, "Hemorphite": 0.48, "Hedbergite": 1.38, "Isogen": 0}
 
 
 class Nocxium(Mineral):
-    ore_dict= {"Pyroxeres": 0.09, "Dark Orchre": 0.13,  "Hemorphite": 0.04,
-               "Hedbergite": 0.03, "Jaspet": 0.14, "Crokite": 0.28, "Nocxium": 0}
+    ore_dict = {"Pyroxeres": 0.09, "Dark Orchre": 0.13,  "Hemorphite": 0.04,
+                "Hedbergite": 0.03, "Jaspet": 0.14, "Crokite": 0.28, "Nocxium": 0}
 
 
 class Zydrine(Mineral):
-    ore_dict= {"Hemorphite": 0.15, "Hedbergite": 0.04,"Jaspet": 0.17, "Crokite": 0.29, "Bistot": 0.11, "Zydrine": 0}
+    ore_dict = {"Hemorphite": 0.15, "Hedbergite": 0.04,"Jaspet": 0.17, "Crokite": 0.29, "Bistot": 0.11, "Zydrine": 0}
 
 
 class Megacyte(Mineral):
-    ore_dict= {"Bistot": 0.24, "Arkonor": 0.31, "Megacyte": 0}
+    ore_dict = {"Bistot": 0.24, "Arkonor": 0.31, "Megacyte": 0}
 
 
 class Morphite(Mineral):
-    ore_dict= {"Mercoxit": 0.18, "Morphite": 0}
+    ore_dict = {"Mercoxit": 0.18, "Morphite": 0}
 
 
 # Dictionary with all the mineral classes to be able to iterate later on
